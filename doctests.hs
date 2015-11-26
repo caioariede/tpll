b19905806e0ec91e3f6f479617bdf83aa5aa30e4
Caio Ariede <caio.ariede@gmail.com>
2015-11-26 00:28:26 -0200

    cleanup

--- doctests.hs
+++
@@ -1,7 +0,0 @@
-import Test.DocTest (doctest)
-import System.FilePath.Glob (globDir, compile)
-import Data.List (isSuffixOf)
-
-main = do
-    (_, paths) <- globDir [compile "*.hs"] "src"
-    doctest $ ["-isrc"] ++ (filter (isSuffixOf ".hs") paths)
